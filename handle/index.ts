c0da7c74c11d5369f5b172d4b1782b14ddb57e8c
lsp <lsp@me.com>
2023-08-04 08:59:52 +0800

    feat: add news reuters source

--- handle/index.ts
+++ handle/index.ts
@@ -2,8 +2,15 @@ import { Browser } from 'puppeteer'
 
 import google from './google'
 import bloomberg from './bloomberg'
+import reuters from './reuters'
 
 export default async (browser: Browser) => [
     await google(browser),
-    await bloomberg(browser)
+    await bloomberg(browser),
+    await reuters(browser)
 ].flat()
+
+// export default async (browser: Browser) => Promise.all([
+//     google(browser),
+//     bloomberg(browser)
+// ]).then(data => data.flat())
---
+++ handle/reuters/index.ts
@@ -0,0 +1,93 @@
+import url from 'node:url'
+import { Browser, Page } from 'puppeteer'
+import dayjs from 'dayjs'
+
+import { getHash, deduplicate } from '../function'
+import { New } from '../interface'
+
+import urls from './urls.json' assert { type: 'json' }
+
+interface RawNew {
+    link: string
+    title: string
+}
+
+const medium = '路透社'
+
+export default async function(browser: Browser) {
+    /** only one page **/
+    // const page = await browser.newPage()
+    // const news: RawNew[][] = []
+
+    // for (const url of urls) {
+    //     await page.goto(url, { timeout: 0 })
+
+    //     news.push(await page.evaluate(() => ([
+    //         Array.from(document.querySelectorAll('h3[data-testid="Heading"] a')),
+    //         Array.from(document.querySelectorAll('a[data-testid="Heading"]'))
+    //     ].flat() as HTMLLinkElement[]).map(a => ({
+    //         link: a.href,
+    //         title: a.innerText
+    //     }))))
+    // }
+
+
+
+    /** multiple page **/
+    // const pagesNum = 3
+    // const taskList = [...urls]
+    // const news: RawNew[][] = []
+
+    // await Promise.all(new Array(pagesNum).fill(undefined).map(() => new Promise(async resolve => task(await browser.newPage(), taskList, news, resolve))))
+
+
+
+    /** open all pages **/
+    const news: RawNew[][] = await Promise.all(urls.map(async url => {
+        const page = await browser.newPage()
+
+        await page.goto(url, { timeout: 0, waitUntil: 'domcontentloaded' })
+
+        const data = await page.evaluate(() => ([
+            Array.from(document.querySelectorAll('h3[data-testid="Heading"] a')),
+            Array.from(document.querySelectorAll('a[data-testid="Heading"]'))
+        ].flat() as HTMLLinkElement[]).map(a => ({
+            link: a.href,
+            title: a.innerText
+        })))
+
+        await page.close()
+        return data
+    }))
+
+
+
+
+    let data: New[] = news.flat().map(({ link, title }) => ({
+        link,
+        title,
+        medium,
+        hash: getHash(medium, title),
+        date: dayjs().format('YYYY-MM-DD HH:mm:00'),
+        tags: '',
+        status: 0
+    }))
+
+    return await deduplicate(data)
+}
+
+// async function task(page: Page, tasks: string[], container: RawNew[][], cb: (arg: any) => void) {
+//     if (!tasks.length) return cb(await page.close())
+
+//     await page.goto((tasks.shift() as string), { timeout: 0 })
+
+//     container.push(await page.evaluate(() => ([
+//         Array.from(document.querySelectorAll('h3[data-testid="Heading"] a')),
+//         Array.from(document.querySelectorAll('a[data-testid="Heading"]'))
+//     ].flat() as HTMLLinkElement[]).map(a => ({
+//         link: a.href,
+//         title: a.innerText
+//     }))))
+
+//     task(page, tasks, container, cb)
+// }
