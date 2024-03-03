d0db9731c7573dd83fd1cb274778b19e69250658
lsp <lsp@me.com>
2024-03-03 15:07:28 +0800

    modify: 'handle' was renamed 'overseas'

--- index.ts
+++ index.ts
@@ -1,18 +1,18 @@
 import * as model from './model'
-import handle from './handle'
+import overseas from './overseas'
 import daddy from './daddy'
-import BigNews from './BigNews'
+// import BigNews from './BigNews'
 
 (async () => {
-    const news = await handle()
-    await model.news.saveNews(news)
-    console.log(`\nadd ${news.length} news`)
+    const overseasNews = await overseas()
+    await model.news.saveNews(overseasNews)
+    console.log(`\nadd ${overseasNews.length} overseas news`)
 
     const daddyInfo = await daddy()
     await model.daddy.saveInfo(daddyInfo)
     console.log(`add ${daddyInfo.length} daddy info\n`)
 
     await model.close()
-    BigNews(news)
+    // BigNews(news)
     process.exit(0)
 })()
