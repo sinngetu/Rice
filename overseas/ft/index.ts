b02bbd7c17594342394423f18b3a43169abd02c0
lsp <lsp@me.com>
2024-06-26 13:49:30 +0800

    fix: change the FT conf

--- overseas/ft/index.ts
+++ overseas/ft/index.ts
@@ -22,28 +22,28 @@ export default async function(browser: Browser) {
         const media = await model.media.getMedia()
         const CNMedium = media.reduce((result, medium) => CNDomain === medium.domain ? medium.id : result, 0)
         const MainMedium = media.reduce((result, medium) => MainDomain === medium.domain ? medium.id : result, 0)
-    
+
         const news: RawNews[][] = await Promise.all(urls.map(async url => {
             const page = await browser.newPage()
-    
-            await page.goto(url, { timeout: 0, waitUntil: 'domcontentloaded' })
-    
+
+            await page.goto(url, { timeout: 0 })
+
             const data = await page.evaluate(() => {
                 const isCN = window.location.host === 'm.ftchinese.com'
                 const CNQuery = 'a.item-headline-link'
                 const MainQuery = 'a.js-teaser-heading-link'
-    
+
                 return (Array.from(document.querySelectorAll(isCN ? CNQuery : MainQuery)) as HTMLLinkElement[]).map(a => ({
                     isCN,
                     link: a.href.split('?')[0],
                     title: a.innerText
                 }))
             })
-    
+
             await page.close()
             return data
         }))
-    
+
         data = news.flat().map(({ isCN, link, title }) => ({
             link,
             title,
