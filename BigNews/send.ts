fb92ad2ed2047d514187e8aa0c1927ae8b460dd3
sinngetu <v.sinngetu@gmail.com>
2024-09-10 19:48:07 +0800

    modify: from named pipe to local socket

--- BigNews/send.ts
+++ BigNews/send.ts
@@ -1,14 +1,13 @@
 import net from 'node:net'
 import Debug from 'debug'
 
-const PIPE_NAME = 'BigNews'
-const PIPE_PATH = '\\\\.\\pipe\\' + PIPE_NAME
-const debug = Debug(`client-pipe:${PIPE_NAME}`)
+const PORT = 9503
+const debug = Debug(`local-socket:${PORT}`)
 let client: net.Socket
 
 export default function(data: string) {
-    try { client = net.connect(PIPE_PATH, () => debug('server connected!')) }
-    catch(e) { debug('failed to connect server!') }
+    try { client = net.connect(PORT, '127.0.0.1', () => debug('server connected!')) }
+    catch(e) { debug(e); console.error('failed to connect server!') }
 
     if (!client) return
     client.write(data)
